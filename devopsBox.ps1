283ea1e4cf836dd20834d01ac5802d42df9be672
MaximoDiaz1990 <77713582+MaximoDiaz1990@users.noreply.github.com>
2024-04-15 22:11:04 -0600

    Update devopsBox.ps1

--- devopsBox.ps1
+++ devopsBox.ps1
@@ -2,6 +2,8 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 
 #Install Software
+choco install office -y
+choco install nodejs-lts  -y
+choco install java -y
 choco install visualstudiocode -y
 choco install git -y
-choco install nodejs-lts  -y
