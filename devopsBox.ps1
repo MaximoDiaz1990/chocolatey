209c1ed80e4bb1734dedeb8ade068d4425388434
MaximoDiaz1990 <77713582+MaximoDiaz1990@users.noreply.github.com>
2024-04-15 23:27:58 -0600

    Update devopsBox.ps1

--- devopsBox.ps1
+++ devopsBox.ps1
@@ -8,4 +8,16 @@ choco install javaruntime -y
 choco install visualstudiocode -y
 choco install git -y
 
-echo %JAVA_HOME%
+# Define the path to the Java installation directory
+$javaInstallationDir = "C:\Program Files\Java\jdk1.8.0_391"
+
+# Set JAVA_HOME environment variable
+[Environment]::SetEnvironmentVariable("JAVA_HOME", $javaInstallationDir, [System.EnvironmentVariableTarget]::Machine)
+
+# Add Java bin directory to the PATH environment variable
+$newPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine) + ";$javaInstallationDir\bin"
+[System.Environment]::SetEnvironmentVariable("Path", $newPath, [System.EnvironmentVariableTarget]::Machine)
+
+# Verify JAVA_HOME and PATH
+Write-Host "JAVA_HOME is set to: $javaInstallationDir"
+Write-Host "PATH is updated with Java bin directory"
